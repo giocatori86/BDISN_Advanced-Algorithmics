c7cb5433a59d0351b9518dbb42a84820f7c51af8
giocatori86 <skk420@few.vu.nl>
2015-11-17 17:25:44 +0100

    first Comments
    commented!

--- random_gradient_group1.m
+++ random_gradient_group1.m
@@ -2,40 +2,52 @@ load EmpiricalData2.mat;
 
 Diagonal_Random = 0.02;
 
-% progress of error to plot
+% Number of iterations to find best Accurancy
 epochs = 10000;
+
+% progress of error to plot
 error = zeros(1, epochs);
 
 % Parameter initialization: weights (48)
-
 para_values = rand(1, 48);
+
+% Calculate SSR for the para values on EmpericalData2 set
 ssr_now = calculate_SSR( para_values, EmpiricalData2 );
+
+% Initialize Count Variable
 count = 1;
 
 while (count <= epochs)
-    % Current value of the sum of squared of residulas: SSRnow
-    
     error(count) = ssr_now;
+    % Temporarly Para Values are Para_values + Randomized value between 0
+    % and 1 * Diagonal Random
     temp_para_values = para_values + (rand(1,48)-0.5) * Diagonal_Random   ; 
         
+        % For every element of para_values adjust with temp_para_values)
         for i = 1 : length(para_values)
-            %sensitivity_values(i) = calculate_param_sensitivity( para_values, i, paramter_step, actualFeelData, ssr_now );        
                     temp_para_values(i) = max(0, min(1, temp_para_values(i) ));
            
         end
+         % Calculate temporarly SSR with the temp para_values on ReferenceSet 
         temp_ssr = calculate_SSR(temp_para_values,EmpiricalData2);
+         
+        % Calculate Delta with temporarly SSR - SSR_now
         delta = temp_ssr - ssr_now ;
-        
+        % If Delta < 0
         if ( delta < 0)
             para_values = temp_para_values ;
+            
+            % ssr_now becomes temporarly SSR 
             ssr_now = temp_ssr  ;
 
         end
     count = count + 1;
 end
 
+% Make Plot
 x = (1: 1: count-1);
 plot(x, error);
 xlabel('epochs');
 ylabel('SSR');
+title({'Plot of Error within Weights'});
 disp(para_values);
